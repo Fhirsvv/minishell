3c0b5ab967a65184114fdcee7947b12e350b5d7e
ecortes- <ecortes-@IdeapadEdu>
2024-07-23 16:19:30 +0200

    ea mas o menos funcionan las comillas y se divide en tokens, falta hacer tokens con |

--- src/lexer/tokens.c
+++ src/lexer/tokens.c
@@ -6,7 +6,7 @@
 /*   By: ecortes- <ecortes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 12:08:06 by ecortes-          #+#    #+#             */
-/*   Updated: 2024/07/23 15:42:54 by ecortes-         ###   ########.fr       */
+/*   Updated: 2024/07/23 16:16:42 by ecortes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int token_type(char *str)
 		type = REDIR_I;
 	else if (strcmp(str, "<<") == 0)
 		type = HERE_DOC;
-	else if(str[0] == '\"')
+	else if (str[0] == '\"')
 		type = D_QUOTE;
 	else
 		type = WORD;
@@ -60,47 +60,57 @@ void new_tokkenice(char *prompt, t_myshell *tshell)
 
 void tokens_and_quotes(char *prompt, t_myshell *tshell)
 {
-    char *start_q = NULL;
-    char *end_q = NULL;
-    char *buff = NULL;
-    int i = 0;
-    bool in_quotes = false;
-    char quote_char = '\0';
+	char *start_q = NULL;
+	char *end_q = NULL;
+	char *buff = NULL;
+	int i = 0;
+	bool in_quotes = false;
+	char quote_char = '\0';
 
-    while (prompt[i]) {
-        if (prompt[i] == ' ' && !in_quotes) {
-            if (start_q != NULL) {
-                end_q = &prompt[i];
-                buff = ft_substr(prompt, start_q - prompt, end_q - start_q);
-                ft_tokenadd_back(&tshell->tokens, ft_token_new(buff, token_type(buff)));
-                printf("TOKEN CONTENT=%s\n", buff);
-                free(buff);
-                start_q = NULL;
-            }
-        } else if (prompt[i] == '\'' || prompt[i] == '"') {
-            if (in_quotes && prompt[i] == quote_char) {
-                in_quotes = false;
-                quote_char = '\0';
-            } else if (!in_quotes) {
-                in_quotes = true;
-                quote_char = prompt[i];
-                if (start_q == NULL) {
-                    start_q = &prompt[i + 1];
-                }
-            }
-        } else {
-            if (start_q == NULL) {
-                start_q = &prompt[i];
-            }
-        }
-        i++;
-    }
-
-    if (start_q != NULL) {
-        end_q = &prompt[i];
-        buff = ft_substr(prompt, start_q - prompt, end_q - start_q);
-        ft_tokenadd_back(&tshell->tokens, ft_token_new(buff, token_type(buff)));
-        printf("TOKEN CONTENT=%s\n", buff);
-        free(buff);
-    }
+	while (prompt[++i])
+	{
+		if (prompt[i] == ' ' && !in_quotes)
+		{
+			if (start_q != NULL)
+			{
+				end_q = &prompt[i];
+				buff = ft_substr(prompt, start_q - prompt, end_q - start_q);
+				ft_tokenadd_back(&tshell->tokens, ft_token_new(buff, token_type(buff)));
+				printf("TOKEN CONTENT=%s\n", buff);
+				free(buff);
+				start_q = NULL;
+			}
+		}
+		else if (prompt[i] == '\'' || prompt[i] == '"')
+		{
+			if (in_quotes && prompt[i] == quote_char)
+			{
+				in_quotes = false;
+				quote_char = '\0';
+				end_q = &prompt[i];
+				buff = ft_substr(prompt, start_q - prompt, end_q - start_q);
+				ft_tokenadd_back(&tshell->tokens, ft_token_new(buff, token_type(buff)));
+				free(buff);
+				start_q = NULL;
+			}
+			else if (!in_quotes)
+			{
+				in_quotes = true;
+				quote_char = prompt[i];
+				start_q = &prompt[i + 1];
+			}
+		}
+		else
+		{
+			if (start_q == NULL)
+				start_q = &prompt[i];
+		}
+	}
+	if (start_q != NULL)
+	{
+		end_q = &prompt[i];
+		buff = ft_substr(prompt, start_q - prompt, end_q - start_q);
+		ft_tokenadd_back(&tshell->tokens, ft_token_new(buff, token_type(buff)));
+		free(buff);
+	}
 }
