576c971c5bb4fc22fa900450d98bc3d7e2656a65
ecortes- <ecortes-@IdeapadEdu>
2024-07-28 21:56:43 +0200

    tokens_and_quotes separado en funciones pequeñas

--- src/lexer/tokens.c
+++ src/lexer/tokens.c
@@ -6,11 +6,58 @@
 /*   By: ecortes- <ecortes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 12:08:06 by ecortes-          #+#    #+#             */
-/*   Updated: 2024/07/28 21:29:16 by ecortes-         ###   ########.fr       */
+/*   Updated: 2024/07/28 21:48:29 by ecortes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+void handle_space(char *prompt, int *i, char **start_q, char quote_char, t_myshell *tshell)
+{
+	if (prompt[*i] == ' ' && quote_char == '\0' && *start_q)
+	{
+		add_token_and_free(*start_q, &prompt[*i], prompt, tshell);
+		*start_q = NULL;
+	}
+}
+
+void handle_quotes(char *prompt, int *i, char *quote_char, char **start_q, t_myshell *tshell)
+{
+	if (prompt[*i] == '\'' || prompt[*i] == '"')
+	{
+		if (*quote_char == prompt[*i])
+		{
+			add_token_and_free(*start_q, &prompt[*i], prompt, tshell);
+			*start_q = NULL;
+			*quote_char = '\0';
+		}
+		else if (*quote_char == '\0')
+		{
+			*quote_char = prompt[*i];
+			if (*start_q)
+				add_token_and_free(*start_q, &prompt[*i], prompt, tshell);
+			*start_q = &prompt[*i + 1];
+		}
+	}
+}
+
+void handle_pipe(char *prompt, int *i, char *quote_char, char **start_q, t_myshell *tshell)
+{
+	if (prompt[*i] == '|')
+	{
+		if (*quote_char == '\0')
+		{
+			if (*start_q)
+			{
+				add_token_and_free(*start_q, &prompt[*i], prompt, tshell);
+				*start_q = NULL;
+			}
+			add_token_and_free(&prompt[*i], &prompt[*i + 1], prompt, tshell);
+		}
+		else if(!*start_q)
+			*start_q = &prompt[*i];
+	}
+}
 
 void tokens_and_quotes(char *prompt, t_myshell *tshell)
 {
@@ -23,51 +70,11 @@ void tokens_and_quotes(char *prompt, t_myshell *tshell)
 	quote_char = '\0';
 	while (prompt[i])
 	{
-		if (prompt[i] == ' ' && quote_char == '\0' && start_q)
-		{
-				add_token_and_free(start_q, &prompt[i], prompt, tshell);
-				start_q = NULL;
-		}
-		else if (prompt[i] == '\'' || prompt[i] == '"')
-		{
-			if (quote_char == prompt[i])
-			{
-				add_token_and_free(start_q, &prompt[i], prompt, tshell);
-				start_q = NULL;
-				quote_char = '\0';
-			}
-			else if (quote_char == '\0')
-			{
-				quote_char = prompt[i];
-				if (start_q)
-				{
-					add_token_and_free(start_q, &prompt[i], prompt, tshell);
-				}
-				start_q = &prompt[i + 1];
-			}
-		}
-		else if (prompt[i] == '|')
-		{
-			if (quote_char == '\0')
-			{
-				if (start_q)
-				{
-					add_token_and_free(start_q, &prompt[i], prompt, tshell);
-					start_q = NULL;
-				}
-				add_token_and_free(&prompt[i], &prompt[i + 1], prompt, tshell);
-			}
-			else
-			{
-				if (!start_q)
-					start_q = &prompt[i];
-			}
-		}
-		else
-		{
-			if (!start_q)
+		handle_space(prompt, &i, &start_q, quote_char, tshell);
+		handle_quotes(prompt, &i, &quote_char, &start_q, tshell);
+		handle_pipe(prompt, &i, &quote_char, &start_q, tshell);
+		if (prompt[i] != ' ' && prompt[i] != '\'' && prompt[i] != '"' && prompt[i] != '|' && !start_q)
 				start_q = &prompt[i];
-		}
 		i++;
 	}
 	if (start_q)
